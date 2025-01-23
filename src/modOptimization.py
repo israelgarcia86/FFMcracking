c0316e95d7026c65095df52031abd1a8ec78202c
israelgarcia86 <israelgarcia@us.es>
2025-01-23 21:04:56 +0100

    add a file for the optimiization module

---
+++ src/modOptimization.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Module containing all the definitions necessary to precit a crack onset using FFM
+Created on Thu Jan 23 18:18:37 2025
+@author: israelgarcia86
+HOW TO USE: Please see Readme in the same repository
+"""
+
+
